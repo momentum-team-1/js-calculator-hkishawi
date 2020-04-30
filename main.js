80a0780e0e267bec1d981e5bd6461eebcd03e0db
hkishawi <hkishawi@gmail.com>
2020-04-29 23:17:04 -0400

    some js no output

---
+++ main.js
@@ -0,0 +1,55 @@
+//REFERENCE
+// let existingElement = document.querySelector(".empty-div");
+// // First select where you want to place the new content
+// let newElement = document.createElement("div");
+// // Create the new element
+// let text = document.createTextNode("Hello there!");
+// // Create some text
+// newElement.appendChild(text);
+// // Add text to new element
+// existingElement.appendChild(newElement);
+// // Add that element to the DOM
+
+//when you hit 7. 7 will appear in input
+
+
+//TRIAL 1
+// const numeral = "seven"
+
+// window.addEventListener ('load', function () {
+//     const parentEl = document.querySelector(``)
+// }
+
+// let seven = document.querySelector("input") 
+//     let seven = document.createElement("seven")
+//     let text = document.createTextNode("7");
+// newElement.apprehendChild("7")
+
+// let seven = document.querySelector("clear")
+//     let seven = document.querySelectorAll("seven")[7];
+//     elementtoRemove.remove();
+
+// TRIAL 2 #7
+//I want to grab #7
+// $(".numbutt").click(function(event) {
+//     var thingClicked = document.querySelector
+//     console.log("you clicked: ", thingClicked)
+//     $("output").html(thingClicked)
+
+// })
+
+//TRIAL 3 #7
+//now that 7s grabbed, need to move it to "output" which prbly means function(event)
+//function move(seven)
+let number = document.getElementById("seven")
+//grabbing the number from the DOM
+let newElement = document.createElement("clear");
+let text = document.createTextNode("7");
+newElement.apprehendChild(text);
+existingElement.apprehendChild("clear");
+
+let elementtoRemove = document.querySelectorAll("clear")[7] //<what is 7 representing? is clear in the correct place??
+
+// const buttonElement = document.getElementById("seven")
+// buttonElement.addEventListener('click', function (event)) {
+//     consolelog(7)
