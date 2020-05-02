925977d2c84ae0fbfdcc6a304a4eb09a16c1311f
hkishawi <hkishawi@gmail.com>
2020-05-02 17:04:25 -0400

    added general funtions

--- main.js
+++ main.js
@@ -16,40 +16,96 @@
 //TRIAL 1
 // const numeral = "seven"
 
-// window.addEventListener ('load', function () {
-//     const parentEl = document.querySelector(``)
-// }
+// // window.addEventListener ('load', function () {
+// //     const parentEl = document.querySelector(``)
+// // }
 
 // let seven = document.querySelector("input") 
 //     let seven = document.createElement("seven")
 //     let text = document.createTextNode("7");
-// newElement.apprehendChild("7")
+// newElement.appendChild("7")
 
 // let seven = document.querySelector("clear")
 //     let seven = document.querySelectorAll("seven")[7];
 //     elementtoRemove.remove();
 
 // TRIAL 2 #7
-//I want to grab #7
+// //I want to grab #7
 // $(".numbutt").click(function(event) {
-//     var thingClicked = document.querySelector
-//     console.log("you clicked: ", thingClicked)
+//     var thingClicked = document.querySelector("seven")
+//     console.log("you clicked: " + event, thingClicked)
 //     $("output").html(thingClicked)
 
 // })
+// //^ comes out undefined...
 
 //TRIAL 3 #7
 //now that 7s grabbed, need to move it to "output" which prbly means function(event)
 //function move(seven)
-let number = document.getElementById("seven")
-//grabbing the number from the DOM
-let newElement = document.createElement("clear");
-let text = document.createTextNode("7");
-newElement.apprehendChild(text);
-existingElement.apprehendChild("clear");
+// function input(numeral) {
+    // let numeral = document.getElementById("#seven");
+    // //grabbing the number from the DOM
+    // let newElement = document.createElement("#output");
+    // let text = document.createTextNode("7");
+    // newElement.appendChild(text);
+    // existingElement.appendChild(newElement);
+// }
+// let elementtoRemove = document.querySelectorAll("clear")[7] //<what is [] representing? is clear in the correct place??
+//^ this code w syntax errors; 
 
-let elementtoRemove = document.querySelectorAll("clear")[7] //<what is 7 representing? is clear in the correct place??
+// //TRIAL 4 #7
+// const keys = document.querySelector('.numbutt');
+// keys.addEventListener('click', (event) => {
+//     const {target} = event; 
+//     if (!target.matches('numbutt')) {
+//         return;
+//     }
+//     if (target.classList.contains('seven')) {
+//         console.log('seven', target.value);
+//         return;
+//     }
+let currentInput = ""
+    
 
-// const buttonElement = document.getElementById("seven")
-// buttonElement.addEventListener('click', function (event)) {
-//     consolelog(7)
+const sevenElement = document.getElementById("seven")
+sevenElement.addEventListener('click', function (event) {
+    console.log("number 7")
+let output = document.querySelector('#output')
+    currentInput = currentInput + "7"
+    output.textContent = currentInput
+})
+
+const plusElement = document.getElementById("plus")
+plusElement.addEventListener('click', function (event) {
+    console.log("x + y")
+let output = document.querySelector("#output") 
+    currentInput = currentInput + "+"
+    output.textContent = currentInput 
+})
+
+const equalElement = document.getElementById ("equal")
+equalElement.addEventListener('click', function (event) {
+    console.log("evaluate")
+    let output = document.querySelector("#output")
+        currentInput=String(eval(currentInput))
+        output.textContent = currentInput
+})   
+
+const clearElement = document.getElementById("clear")
+clearElement.addEventListener('click', function (event) {
+    console.log("clear all")
+    let output = document.querySelector ("#output")
+        currentInput = ""
+        output.textContent = currentInput
+})
+
+//goal: take input and evaluate it with function, display results of eval function
+
+    
+ 
+
+    // let parentEl = document.querySelector('#seven')
+    // letSevenEl = document.createElement('div')
+    // console.log("inputting 7")
+    // let text = document.createTextNode("7")
+    
